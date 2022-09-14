57c7f5c6a762a0618f0688b3a61256cddecae893
Olabode <bode.ladejobi@gmail.com>
2022-09-14 23:15:22 +0100

    seventh commit

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -1,3 +1,59 @@
 <x-layout>
+    <x-card class="p-10">
+        <header>
+            <h1
+                class="text-3xl text-center font-bold my-6 uppercase"
+            >
+                Manage Gigs
+            </h1>
+        </header>
 
+        <table class="w-full table-auto rounded-sm">
+            <tbody>
+                @unless ($listings->isEmpty())
+                @foreach ($listings as $listing)
+                <tr class="border-gray-300">
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a href="show.html">
+                            {{$listings->title}}
+                        </a>
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a
+                            href="edit.html"
+                            class="text-blue-400 px-6 py-2 rounded-xl"
+                            ><i
+                                class="fa-solid fa-pen-to-square"
+                            ></i>
+                            Edit</a
+                        >
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <form action="">
+                            <button class="text-red-600">
+                                <i
+                                    class="fa-solid fa-trash-can"
+                                ></i>
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+                @else
+                    <tr class="border-gray-300">
+                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                            <p class="text-center">No listings found</p>
+                        </td>
+                    </tr>
+                @endunless
+            </tbody>
+        </table>
+    </x-card>
 </x-layout>
