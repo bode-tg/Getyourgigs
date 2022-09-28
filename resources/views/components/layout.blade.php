7e88b6aa5adcafc3bdbc4ac920784a769a87b120
Olabode <bode.ladejobi@gmail.com>
2022-09-28 20:20:53 +0100

    added some new features to the footer and search

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -72,7 +72,7 @@
             {{$slot}}
         </main>
         <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-orange-500 text-white h-24 mt-24 opacity-90 md:justify-center"
     >
         <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
