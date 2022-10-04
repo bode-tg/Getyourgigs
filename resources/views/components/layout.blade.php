d35d7c4a353fa86ddc1f58624b14dbc3e0285aee
Olabode <bode.ladejobi@gmail.com>
2022-10-04 22:33:41 +0100

    logo and pagination changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,7 +30,7 @@
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="/"
-                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
+                ><img class="w-24 h-13" src="{{asset('images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 @auth
--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -2,7 +2,7 @@
     <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 hover:border-black bg-white border-2 border-gray-300 cursor-default leading-5 rounded-md">
+            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 hover:text-black hover:border-black bg-white border-2 border-gray-300 cursor-default leading-5 rounded-md">
                 {!! __('pagination.previous') !!}
             </span>
         @else
@@ -13,7 +13,7 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border-2 border-gray-300 hover:border-black leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border-2 border-gray-300 hover:border-black leading-5 rounded-md hover:text-black focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                 {!! __('pagination.next') !!}
             </a>
         @else
